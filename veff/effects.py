a8b715ced1365ed1af86df8f252e563cd6a45715
RazmusAnderson <computerchris101@gmail.com>
2022-02-20 16:39:49 +0000

    add broken frame differencing

--- veff/effects.py
+++ veff/effects.py
@@ -1,9 +1,21 @@
 ''' Video effects '''
 
-def none(frames, config):
-    ''' Null effect '''
-    return frames
+import numpy as np
+
+import log
+
+def frame_difference(frames, config):
+    ''' Applies a frame differencing effect to the passed frames '''
+    pbar = log.progress_bar(len(frames), 'Applying frame differencing', 'frames')
+    previous = frames[0]
+    newFrames = []
+    for frame in frames[1:-1]: # final frame will be of type None
+        newFrames.append(np.uint8(np.absolute(np.subtract(np.int16(previous), np.int16(frame)))))
+        # frame = np.uint8(np.bitwise_and(previous, frame))
+        previous = frame
+        pbar.update()
+    return newFrames
 
 EFFECTS = {
-    'none': none
+    'frame_difference': frame_difference
 }
